44b62434d215aaa47e33a1191280cc4e1c4d5099
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2024-10-12 17:40:18 +0200

    read TSP problem instances from X,Y,cost CSV file

---
+++ src/tsp/instances.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from . import config
+
+
+def read_instance_xyv_df(file_name: str | Path):
+    inp = pd.read_csv(file_name, sep=";", header=None, names=["x", "y", "v"])
+    # inp.columns = ["x", "y", "v"]
+    # assert inp.shape == (200, 3)
+    # print(inp.describe())
+    return inp
+
+
+def read_instance_xyv(file_name: str | Path) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    inp = read_instance_xyv_df(file_name)
+    node_costs = np.asarray(inp.y)
+    inp_points = np.asarray([inp.x, inp.y]).T
+    assert inp_points.shape[1] == 2
+    # TODO: check int64
+
+    # compute distance matrix
+    D = inp_points[:, None] - inp_points[None, :]
+    D = np.sqrt((D**2).sum(-1))
+    # round to nearest integer
+    D = np.floor(D + 0.5).astype(np.int64)
+
+    return node_costs, inp_points, D
