97ce3372d2ca263b8b78d2d1c6ece7d8e7f02a45
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2024-10-15 20:30:01 +0200

    refactor to `evalWithStarting` function

--- main.cpp
+++ main.cpp
@@ -20,6 +20,10 @@ using std::endl;
 using std::string;
 using std::vector;
 
+// func(distanceMatrix, costs) returning sequence of indices
+using TSPSolver = vector<int>(vector<vector<int>>, vector<int>);
+using TSPSolverStarting = vector<int>(vector<vector<int>>, vector<int>, int);
+
 void printResults(vector<int> sol, bool toFile = false,
                   const char* filename = "solution.txt")
 {
@@ -125,20 +129,24 @@ std::vector<int> greedyCycleTSP(std::vector<std::vector<int>> distanceMatrix,
     return solution;
 }
 
-void randomTSPTask(vector<vector<int>> distanceMatrix, vector<int> costs)
+void evalWithStarting(vector<vector<int>> distanceMatrix, vector<int> costs,
+                      TSPSolverStarting solver, const char* name)
 {
     vector<int> sol;
     int value;
 
     vector<int> best_sol;
-    int best_sol_value = 1000000;
+    int best_sol_value = 1'000'000;
 
     vector<int> worst_sol;
     int worst_sol_value = 0;
 
     // Generating 200 random solutions, keeping the best and the worst ones.
     for (int i = 0; i < 200; i++) {
-        sol = randomTSP(distanceMatrix, costs);
+        // sol = nearestNeighborTSP(distanceMatrix, costs, i);
+        // sol = nearestNeighborAnyTSP(distanceMatrix, costs, i);
+        // {       sol = greedyCycleTSP(distanceMatrix, costs, i);
+        sol = solver(distanceMatrix, costs, i);
         value = evaluateSolution(distanceMatrix, costs, sol);
         if (value < best_sol_value) {
             best_sol_value = value;
@@ -151,102 +159,13 @@ void randomTSPTask(vector<vector<int>> distanceMatrix, vector<int> costs)
     }
 
     cout << "BEST: " << best_sol_value << endl;
-    printResults(best_sol, true, "random/best.txt");
+    std::stringstream ss;
+    ss << name << "/best.txt";
+    printResults(best_sol, true, ss.str().c_str());
+    ss.clear();
+    ss << name << "/worst.txt";
     cout << "WORST: " << worst_sol_value << endl;
-    printResults(worst_sol, true, "random/worst.txt");
-}
-
-void nearestNeighborTSPTask(vector<vector<int>> distanceMatrix, vector<int> costs)
-{
-    vector<int> sol;
-    int value;
-
-    vector<int> best_sol;
-    int best_sol_value = 1000000;
-
-    vector<int> worst_sol;
-    int worst_sol_value = 0;
-
-    // Generating 200 random solutions, keeping the best and the worst ones.
-    for (int i = 0; i < 200; i++) {
-        sol = nearestNeighborTSP(distanceMatrix, costs, i);
-        value = evaluateSolution(distanceMatrix, costs, sol);
-        if (value < best_sol_value) {
-            best_sol_value = value;
-            best_sol = sol;
-        }
-        if (value > worst_sol_value) {
-            worst_sol_value = value;
-            worst_sol = sol;
-        }
-    }
-
-    cout << "BEST: " << best_sol_value << endl;
-    printResults(best_sol, true, "nearest/best.txt");
-    cout << "WORST: " << worst_sol_value << endl;
-    printResults(worst_sol, true, "nearest/worst.txt");
-}
-
-void nearestNeighborAnyTSPTask(vector<vector<int>> distanceMatrix, vector<int> costs)
-{
-    vector<int> sol;
-    int value;
-
-    vector<int> best_sol;
-    int best_sol_value = 1000000;
-
-    vector<int> worst_sol;
-    int worst_sol_value = 0;
-
-    // Generating 200 random solutions, keeping the best and the worst ones.
-    for (int i = 0; i < 200; i++) {
-        sol = nearestNeighborAnyTSP(distanceMatrix, costs, i);
-        value = evaluateSolution(distanceMatrix, costs, sol);
-        if (value < best_sol_value) {
-            best_sol_value = value;
-            best_sol = sol;
-        }
-        if (value > worst_sol_value) {
-            worst_sol_value = value;
-            worst_sol = sol;
-        }
-    }
-
-    cout << "BEST: " << best_sol_value << endl;
-    printResults(best_sol, true, "nearest_any/best.txt");
-    cout << "WORST: " << worst_sol_value << endl;
-    printResults(worst_sol, true, "nearest_any/worst.txt");
-}
-
-void greedyCycleTSPTask(vector<vector<int>> distanceMatrix, vector<int> costs)
-{
-    vector<int> sol;
-    int value;
-
-    vector<int> best_sol;
-    int best_sol_value = 1000000;
-
-    vector<int> worst_sol;
-    int worst_sol_value = 0;
-
-    // Generating 200 random solutions, keeping the best and the worst ones.
-    for (int i = 0; i < 200; i++) {
-        sol = greedyCycleTSP(distanceMatrix, costs, i);
-        value = evaluateSolution(distanceMatrix, costs, sol);
-        if (value < best_sol_value) {
-            best_sol_value = value;
-            best_sol = sol;
-        }
-        if (value > worst_sol_value) {
-            worst_sol_value = value;
-            worst_sol = sol;
-        }
-    }
-
-    cout << "BEST: " << best_sol_value << endl;
-    printResults(best_sol, true, "nearest/best.txt");
-    cout << "WORST: " << worst_sol_value << endl;
-    printResults(worst_sol, true, "nearest/worst.txt");
+    printResults(worst_sol, true, ss.str().c_str());
 }
 
 int main()
@@ -262,11 +181,18 @@ int main()
     //     cout << std::endl;
     // }
 
-    randomTSPTask(D, costs);
-    nearestNeighborTSPTask(D, costs);
-    nearestNeighborAnyTSPTask(D, costs);
-    greedyCycleTSPTask(D, costs);
-    // vector<int> sol = nearestNeighborAnyTSP(D, costs, 0);
+    cerr << "\x1b[32m random solution \x1b[0m" << endl;
+    // randomTSPTask(D, costs, randomTSP);
+    evalWithStarting(D, costs, randomTSP, "random");
+    cerr << "\x1b[32m  NN task\x1b[0m" << endl;
+    // nearestNeighborTSPTask(D, costs);
+    evalWithStarting(D, costs, nearestNeighborTSP, "NN-end");
+    cerr << "\x1b[32m  NN any TSP\x1b[0m" << endl;
+    // nearestNeighborAnyTSPTask(D, costs);
+    evalWithStarting(D, costs, nearestNeighborAnyTSP, "NN-any");
+
+    cerr << "\x1b[32m  greedy cycle\x1b[0m" << endl;
+    evalWithStarting(D, costs, greedyCycleTSP, "greedyCycle");
 
     // vector<int> sol;
     // sol = nearestNeighborAnyTSP(D, costs, 0);
