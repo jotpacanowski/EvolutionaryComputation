404cad894067461b3a5200093668c18ad65c85da
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2024-10-26 18:54:59 +0200

    remove local functions from header

--- local_search.hpp
+++ local_search.hpp
@@ -2,18 +2,6 @@
 
 #include "headers.hpp"
 
-int intraSwapTwoNodesImpact(const vector<int> &solution, int id1, int id2,
-                            const vector<vector<int>> &distanceMatrix,
-                            const int solution_size);
-
-int intraSwapTwoEdgesImpact(const vector<int> &solution, int id1, int id2,
-                            const vector<vector<int>> &distanceMatrix,
-                            const int solution_size);
-
-int interSwapTwoNodesImpact(const vector<int> &solution, int idx, int external_node,
-                            const vector<vector<int>> &distanceMatrix,
-                            const vector<int> &costs, const int solution_size);
-
 vector<int> localSearch(vector<int> solution, const vector<vector<int>> &distanceMatrix,
                         const vector<int> &costs, const int solution_size,
                         bool edges = false);
